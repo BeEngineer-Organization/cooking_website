a76f6a49b37571e241a187c3a139f8679fae869d
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-09-28 17:50:11 +0900

    全体像の可視化

--- cooking_website/urls.py
+++ cooking_website/urls.py
@@ -4,4 +4,18 @@ from . import views
 
 app_name = "cooking_website"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("sign-up", views.SignUpView.as_view(), name="sign_up"),
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.LogoutView.as_view(), name="logout"),
+    path("search", views.SearchView.as_view(), name="search"),
+    path("recipe", views.RecipeView.as_view(), name="recipe"),
+    path("recipe/create", views.RecipeCreateView.as_view(), name="recipe_create"),
+    path("recipe/update", views.RecipeUpdateView.as_view(), name="recipe_update"),
+    path("user/recipe", views.UserRecipeView.as_view(), name="user_recipe"),
+    path("user/following", views.UserFollowingView.as_view(), name="user_following"),
+    path("user/follower", views.UserFollowerView.as_view(), name="user_follower"),
+    path("user/update", views.UserUpdateView.as_view(), name="user_update"),
+    path("notification", views.NotificationView.as_view(), name="notification"),
+]
--- cooking_website/views.py
+++ cooking_website/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.views.generic import (
+    TemplateView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    ListView,
+)
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    pass
+
+
+class SignUpView(CreateView):
+    pass
+
+
+class LoginView(LoginView):
+    pass
+
+
+class LogoutView(LogoutView):
+    pass
+
+
+class SearchView(TemplateView):
+    pass
+
+
+class RecipeView(DetailView):
+    pass
+
+
+class RecipeCreateView(CreateView):
+    pass
+
+
+class RecipeUpdateView(UpdateView):
+    pass
+
+
+class UserRecipeView(DetailView):
+    pass
+
+
+class UserFollowingView(ListView):
+    pass
+
+
+class UserFollowerView(ListView):
+    pass
+
+
+class UserUpdateView(UpdateView):
+    pass
+
+
+class NotificationView(ListView):
+    pass
