b39b5cbf1feedaf91d2264027bfc62884a3653a4
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-10-02 11:24:39 +0900

    フォームの作成

---
+++ cooking_website/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from .models import CustomUser, Recipe
+
+
+class LoginForm(AuthenticationForm):
+    pass
+
+
+class CustomUserForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "password1", "password2" "profile", "image"]
+
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ["title", "image", "description"]
