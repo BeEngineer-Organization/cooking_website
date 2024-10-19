8f0b1d4c24512a3b1af5ece19a61f065761df34a
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-10-19 19:23:20 +0900

    授業で作成する箇所を削除

--- cooking_website/views.py
+++ cooking_website/views.py
@@ -47,92 +47,11 @@ class MyLogoutView(LoginRequiredMixin, LogoutView):
 
 
 class SearchView(LoginRequiredMixin, TemplateView):
-    template_name = "cooking_website/search.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        form = RecipeSearchForm(self.request.GET)
-        if form.is_valid():
-            keyword = form.cleaned_data["keyword"]
-            if keyword:
-                popular_recipes = (
-                    Recipe.objects.filter(
-                        Q(title__icontains=keyword) | Q(description__icontains=keyword)
-                    )
-                    .annotate(like_count=Count("like"))
-                    .select_related("written_by")
-                    .order_by("-like_count")
-                    .values(
-                        "pk",
-                        "title",
-                        "image",
-                        "description",
-                        "written_by__username",
-                        "written_by__image",
-                    )
-                )
-                new_recipes = (
-                    Recipe.objects.filter(
-                        Q(title__icontains=keyword) | Q(description__icontains=keyword)
-                    )
-                    .select_related("written_by")
-                    .order_by("-created_at")
-                    .values(
-                        "pk",
-                        "title",
-                        "image",
-                        "description",
-                        "written_by__username",
-                        "written_by__image",
-                    )
-                )
-
-        else:
-            popular_recipes = (
-                Recipe.objects.all()
-                .annotate(like_count=Count("like"))
-                .order_by("-like_count")
-                .values(
-                    "pk",
-                    "title",
-                    "image",
-                    "description",
-                    "written_by__username",
-                    "written_by__image",
-                )
-            )
-            new_recipes = (
-                Recipe.objects.all()
-                .select_related("written_by")
-                .order_by("-created_at")
-                .values(
-                    "pk",
-                    "title",
-                    "image",
-                    "description",
-                    "written_by__username",
-                    "written_by__image",
-                )
-            )
-
-        context["form"] = form
-        context["popular_recipes"] = popular_recipes
-        context["new_recipes"] = new_recipes
-        return context
+    pass
 
 
 class RecipeView(LoginRequiredMixin, DetailView):
-    template_name = "cooking_website/recipe.html"
-    model = Recipe
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        recipe = context["object"]
-        context["like_count"] = recipe.like.all().count()
-        context["is_liked"] = bool(
-            Like.objects.filter(recipe=recipe, given_by=self.request.user)
-        )
-        return context
+    pass
 
 
 class RecipeUpdateView(LoginRequiredMixin, UpdateView):
@@ -147,19 +66,7 @@ class RecipeUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class RecipeCreateView(LoginRequiredMixin, CreateView):
-    template_name = "cooking_website/recipe_create.html"
-    model = Recipe
-    form_class = RecipeForm
-
-    def get_success_url(self):
-        return reverse_lazy("cooking_website:recipe", kwargs={"pk": self.recipe_pk})
-
-    def form_valid(self, form):
-        instance = form.save(commit=False)
-        instance.written_by = self.request.user
-        instance.save()
-        self.recipe_pk = instance.pk
-        return super().form_valid(form)
+    pass
 
 
 class UserView(LoginRequiredMixin, DetailView):
@@ -194,77 +101,15 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class UserFollowingView(LoginRequiredMixin, ListView):
-    template_name = "cooking_website/user_following.html"
-    model = Connection
-    paginate_by = 5
-
-    def get_queryset(self):
-        user_pk = self.kwargs.get("pk")
-        user = get_object_or_404(CustomUser, pk=user_pk)
-        query_set = (
-            Connection.objects.filter(follower=user)
-            .all()
-            .select_related("followee")
-            .values("followee__pk", "followee__image", "followee__username")
-        )
-        return query_set
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["user_pk"] = self.kwargs.get("pk")
-        return context
+    pass
 
 
 class UserFollowerView(LoginRequiredMixin, ListView):
-    template_name = "cooking_website/user_followed.html"
-    model = Connection
-    paginate_by = 5
-
-    def get_queryset(self):
-        user_pk = self.kwargs.get("pk")
-        user = get_object_or_404(CustomUser, pk=user_pk)
-        query_set = (
-            Connection.objects.filter(followee=user)
-            .all()
-            .select_related("follower")
-            .values("follower__pk", "follower__image", "follower__username")
-        )
-        return query_set
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["user_pk"] = self.kwargs.get("pk")
-        return context
+    pass
 
 
 class NotificationView(LoginRequiredMixin, ListView):
-    template_name = "cooking_website/notification.html"
-    model = Notification
-    paginate_by = 10
-
-    def get_queryset(self):
-        return (
-            Notification.objects.filter(recipient=self.request.user)
-            .all()
-            .select_related("sender")
-            .order_by("-created_at")
-            .values("content", "sender__pk")
-        )
-
-
-def like_post(request):
-    recipe = get_object_or_404(Recipe, pk=request.POST.get("recipe_pk"))
-    like = Like.objects.filter(recipe=recipe, given_by=request.user)
-
-    if like.exists():
-        like.delete()
-    else:
-        Like.objects.create(recipe=recipe, given_by=request.user)
-
-    context = {
-        "like_count": recipe.like.all().count(),
-    }
-    return JsonResponse(context)
+    pass
 
 
 def follow_post(request):
