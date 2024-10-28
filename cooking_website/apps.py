e0e4281a3aff4ee47f4923e08353ce865611d433
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-10-28 16:39:24 +0900

    不必要な部分を削除

--- cooking_website/apps.py
+++ cooking_website/apps.py
@@ -4,9 +4,3 @@ from django.apps import AppConfig
 class CookingWebsiteConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cooking_website"
-
-    def ready(self):
-        try:
-            from . import signals
-        except ImportError:
-            pass
