eb3e850dc148b26fa1cce717bcc18e1710e9a3e3
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-11-09 11:09:52 +0900

    レビュー対応：CSSとforms.pyの修正

--- cooking_website/forms.py
+++ cooking_website/forms.py
@@ -5,31 +5,16 @@ from .models import CustomUser, Recipe
 
 
 class SignupForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class": "input"})
-        self.fields["password1"].widget.attrs.update({"class": "input"})
-        self.fields["password2"].widget.attrs.update({"class": "input"})
-        self.fields["profile"].widget.attrs.update({"class": "text"})
-
     class Meta:
         model = CustomUser
-        fields = ["username", "password1", "password2", "profile", "image"]
+        fields = ["username", "profile", "image"]
 
 
 class LoginForm(AuthenticationForm):
-    def __init__(self, request, *args, **kwargs):
-        super().__init__(request, *args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class": "input"})
-        self.fields["password"].widget.attrs.update({"class": "input"})
+    pass
 
 
 class UserUpdateForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class": "input"})
-        self.fields["profile"].widget.attrs.update({"class": "text"})
-
     class Meta:
         model = CustomUser
         fields = ["username", "profile", "image"]
@@ -48,9 +33,3 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ["title", "image", "description"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["title"].widget.attrs.update({"class": "input"})
-        self.fields["image"].widget.attrs.update({"class": "input"})
-        self.fields["description"].widget.attrs.update({"class": "text"})
